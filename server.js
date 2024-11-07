cf4250912c5c8bac3947160825a7340a5c44a8ff
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-11-07 08:10:58 +0530

    commit it

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 // Set up Ethereal transporter
 const transporter = nodemailer.createTransport({
   host: 'smtp.ethereal.email',
